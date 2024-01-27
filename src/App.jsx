43b03b324f8ab7334e67cc9038816c9f475e4938
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-01-27 20:48:59 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "bootstrap";
+import LogoStarline from "./assets/logoStarline.png";
+import HeaderComponent from "./components/HeaderComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="row g-3">
+      <HeaderComponent />
+    </div>
+  );
+}
+
+export default App;
