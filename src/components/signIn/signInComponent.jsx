749abf528f772103895fce3daa9349755ee78b34
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-01-29 00:24:38 -0300

    alterações tela de login

--- src/components/signIn/signInComponent.jsx
+++ src/components/signIn/signInComponent.jsx
@@ -1,31 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import LogoStarlineBlue from "../../assets/logoStarlineBlue.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./signInComponent.css";
 
 const SignInComponent = () => {
+  const [password, setPassword] = useState("");
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div>
-      <form className="row">
+      <form className="row full-signIn-container">
         <div className="col-lg-12 col-md-10 col-sm-12">
           <label className="col-lg-2"></label>
           <img className="signIn-image" src={LogoStarlineBlue} />
         </div>
-        <div className="col-lg-7 col-md-10 col-sm-12">
+        <div className="col-lg-12 col-md-10 col-sm-12">
           <label htmlFor="inputEmail" className="form-label">
             Email
           </label>
           <input type="text" className="form-control" id="inputEmail" />
         </div>
-        <div className="col-1"></div>
-        <div className="col-lg-7 col-md-10 col-sm-12">
+        <div className="col-lg-12 col-md-10 col-sm-12">
           <label htmlFor="inputSenha" className="form-label">
             Senha
           </label>
-          <input type="text" className="form-control" id="inputPassword" />
+          <input
+            type="password"
+            value={password.replace(/./g, "*")}
+            onChange={handlePasswordChange}
+            className="form-control"
+            id="inputPassword"
+          />
         </div>
-        <div className="col-lg-7 col-md-10 col-sm-12">
+        <div className="col-lg-12 col-md-10 col-sm-12">
           <button
             type="text"
             className="form-control button"
@@ -34,7 +46,7 @@ const SignInComponent = () => {
             Entrar
           </button>
         </div>
-        <div className="col-lg-7 col-md-10 col-sm-12 centered">
+        <div className="col-lg-12 col-md-10 col-sm-12 centered">
           <Link to="/cadastrar" className="register-text">
             <h5>Não tem conta? Cadastre-se</h5>
           </Link>
