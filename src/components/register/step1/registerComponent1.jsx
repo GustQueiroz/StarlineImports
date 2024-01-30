27c8e77d8dae8dd253d181b444be37256373c2ea
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-01-30 01:05:12 -0300

    tela de cadastro, responsividade completa e configurações

--- src/components/register/step1/registerComponent1.jsx
+++ src/components/register/step1/registerComponent1.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import LogoStarlineBlue from "../../assets/logoStarlineBlue.png";
+import LogoStarlineBlue from "../../../assets/logoStarlineBlue.png";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./signInComponent.css";
+import "./registerComponent1.css";
 
-const SignInComponent = () => {
+const RegisterComponent1 = () => {
   const [password, setPassword] = useState("");
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+  };
+
+  const [repetPassword, setRepetPassword] = useState("");
+
+  const handleRepetPasswordChange = (event) => {
+    setRepetPassword(event.target.value);
   };
 
   return (
@@ -18,12 +24,24 @@ const SignInComponent = () => {
         <div className="col-lg-12 col-md-10 col-sm-12">
           <label className="col-lg-2"></label>
           <Link to="/">
-            <img className="signIn-image" src={LogoStarlineBlue} />
+            <img className="register-image" src={LogoStarlineBlue} />
           </Link>
         </div>
         <div className="col-lg-12 col-md-10 col-sm-12">
           <label htmlFor="inputEmail" className="form-label">
+            Nome Completo
+          </label>
+          <input type="text" className="form-control" id="inputEmail" />
+        </div>
+        <div className="col-lg-12 col-md-10 col-sm-12">
+          <label htmlFor="inputEmail" className="form-label">
             Email
+          </label>
+          <input type="text" className="form-control" id="inputEmail" />
+        </div>
+        <div className="col-lg-12 col-md-10 col-sm-12">
+          <label htmlFor="inputEmail" className="form-label">
+            CPF
           </label>
           <input type="text" className="form-control" id="inputEmail" />
         </div>
@@ -40,25 +58,29 @@ const SignInComponent = () => {
           />
         </div>
         <div className="col-lg-12 col-md-10 col-sm-12">
+          <label htmlFor="inputSenha" className="form-label">
+            Repetir Senha
+          </label>
+          <input
+            type="password"
+            value={repetPassword.replace(/./g, "*")}
+            onChange={handleRepetPasswordChange}
+            className="form-control"
+            id="inputPassword"
+          />
+        </div>
+        <div className="col-lg-12 col-md-10 col-sm-12">
           <button
             type="text"
             className="form-control button"
             id="inputPassword"
           >
-            Entrar
+            Criar
           </button>
-          <Link to="/">
-            <label className="forgot">Esqueceu sua senha?</label>
-          </Link>
-        </div>
-        <div className="col-lg-12 col-md-10 col-sm-12 centered">
-          <Link to="/cadastrar" className="register-text">
-            <h5>Não tem conta? Cadastre-se</h5>
-          </Link>
         </div>
       </form>
     </div>
   );
 };
 
-export default SignInComponent;
+export default RegisterComponent1;
